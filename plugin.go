68a6548030a5038fd7ec594775f8228f2f1468b5
InvalidJoker <0limitdev@gmail.com>
2024-08-05 20:06:32 +0200

    added config system

--- plugin.go
+++ plugin.go
@@ -2,22 +2,44 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"minicloud/cloud"
+	"os"
 
 	"go.minekube.com/gate/pkg/edition/java/proxy"
 )
 
+type Config struct {
+	AuthToken string `json:"auth_token"`
+	Port      int    `json:"port"`
+	Interface string `json:"interface"`
+}
+
 var Plugin = proxy.Plugin{
 	Name: "MiniCloud",
 	Init: func(ctx context.Context, p *proxy.Proxy) error {
-		return nil
+		data, err := os.ReadFile("config.json")
+		if err != nil {
+			return err
+		}
+		var config Config
 
-		dockerService := cloud.NewDockerService()
+		err = json.Unmarshal(data, &config)
 
-		
+		if err != nil {
+			return err
+		}
 
+		dockerService, err := cloud.NewDockerService()
+
+		if err != nil {
+			return err
+		}
+
+		backendService := cloud.NewBackendService(dockerService)
+
+		go backendService.Start()
 
 		return nil
 	},
-
 }
